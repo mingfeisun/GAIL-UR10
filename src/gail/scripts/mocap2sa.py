55cceece884d1e0340dd54c6b58c6fa3eb71fd5b
Mingfei Sun <mingfei.sun@ust.hk>
2018-12-09 00:10:58 +0800

    Update mocap2sa.py

--- src/gail/scripts/mocap2sa.py
+++ src/gail/scripts/mocap2sa.py
@@ -5,7 +5,7 @@ import numpy
 import rospy
 import roslib
 
-import geometry_msgs.msg
+from geometry_msgs.msg import Pose
 
 from bvh_broadcaster import BVHBroadcaster
 
@@ -18,33 +18,26 @@ class Mocap2SA:
         self.tf_listener = tf.TransformListener()
         self.mocap_ee_parent_frame = 'Hips'
         self.mocap_ee_child_frame = 'RightHandIndex1'
+        self.ur10_base_frame = 'base_link'
 
     def __call__(self):
-        frame_matrix = []
-        motion_matrix = []
+        (trans, rot) = self.tf_listener.lookupTransform(self.mocap_ee_child_frame, self.mocap_ee_parent_frame, rospy.Time(0))
+        trans_mat = tf.transformations.translation_matrix(trans)
+        rot_mat = tf.transformations.quaternion_matrix(rot)
+        matrix_b = numpy.matmul(trans_mat, rot_mat)
 
-        for ur10_frame, mocap_frame in zip(self.ur10_frames_parent, self.mocap_frames_parent):
-            try:
-                (trans, rot) = self.tf_listener.lookupTransform(mocap_frame, ur10_frame, rospy.Time(0))
-                trans_mat = tf.transformations.translation_matrix(trans)
-                rot_mat = tf.transformations.quaternion_matrix(rot)
-                frame_matrix.append(numpy.matmul(trans_mat, rot_mat))
-            except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
-                pass
+        (trans, rot) = self.tf_listener.lookupTransform(self.mocap_ee_parent_frame, self.ur10_base_frame, rospy.Time(0))
+        trans_mat = tf.transformations.translation_matrix(trans)
+        rot_mat = tf.transformations.quaternion_matrix(rot)
+        tf_b_s = numpy.matmul(trans_mat, rot_mat)
 
-        for child, parent in zip(self.mocap_frames_child, self.mocap_frames_parent):
-            try:
-                (trans, rot) = self.tf_listener.lookupTransform(mocap_frame, ur10_frame, rospy.Time(0))
-                trans_mat = tf.transformations.translation_matrix(trans)
-                rot_mat = tf.transformations.quaternion_matrix(rot)
-                motion_matrix.append(numpy.matmul(trans_mat, rot_mat))
-            except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
-                pass
+        matrix_s = numpy.matmul(tf_b_s, matrix_b)
 
-        for motion, frame_tf in zip(motion_matrix, frame_matrix):
-            self.converted_motion.append(numpy.matmul(frame_matrix, motion_matrix))
+        posi = tf.transformations.translation_from_matrix(pose_s)
+        orien = tf.transformations.orientation_from_matrix(pose_s)
 
-        print(self.converted_motion)
+        pose_s = Pose(position=posi, orientation=orien)
+        print(pose_s)
 
 if __name__ == "__main__":
     rospy.init_node('mocap2sa')
